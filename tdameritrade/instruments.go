fb4182eb4135bec7a144955dc7a13a06420a226f
zach rice <zricer@protonmail.com>
2020-01-03 21:33:28 -0500

    instruments work

--- tdameritrade/instruments.go
+++ tdameritrade/instruments.go
@@ -2,6 +2,7 @@ package tdameritrade
 
 import (
 	"context"
+	"fmt"
 )
 
 // InstrumentService handles communication with the marketdata related methods of
@@ -12,12 +13,58 @@ type InstrumentService struct {
 	client *Client
 }
 
-func (s *MarketHoursService) GetInstrument(ctx context.Context, cusip string) (*Instrument, *Response, error) {
-	// TODO
+type Instruments map[string]*InstrumentInfo
+
+type InstrumentInfo struct {
+	Cusip       string `json:"cusip,omitempty"`
+	Symbol      string `json:"symbol"`
+	Description string `json:"description,omitempty"`
+	Type        string `json:"assetType"` //"'NOT_APPLICABLE' or 'OPEN_END_NON_TAXABLE' or 'OPEN_END_TAXABLE' or 'NO_LOAD_NON_TAXABLE' or 'NO_LOAD_TAXABLE'"
+	Exchange    string `json:"exchange"`
+}
+
+func (s *InstrumentService) GetInstrument(ctx context.Context, cusip string) (*Instruments, *Response, error) {
+	if cusip == "" {
+		return nil, nil, fmt.Errorf("no cusip present")
+	}
+	u := fmt.Sprintf("instruments/%s", cusip)
+	req, err := s.client.NewRequest("GET", u, nil)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	instruments := new(Instruments)
+
+	resp, err := s.client.Do(ctx, req, instruments)
+	if err != nil {
+		return nil, resp, err
+	}
 	return nil, nil, nil
 }
 
-func (s *MarketHoursService) SearchInstruments(ctx context.Context, symbol, projection string) (*Instrument, *Response, error) {
-	// TODO
-	return nil, nil, nil
+func (s *InstrumentService) SearchInstruments(ctx context.Context, symbol, projection string) (*Instruments, *Response, error) {
+	u := fmt.Sprintf("instruments")
+	if symbol == "" {
+		return nil, nil, fmt.Errorf("no symbol present")
+	}
+	if projection == "" {
+		projection = "symbol-search"
+	}
+	u = fmt.Sprintf("%s?symbol=%s&projection=%s", u, symbol, projection)
+
+	req, err := s.client.NewRequest("GET", u, nil)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	instruments := new(Instruments)
+
+	resp, err := s.client.Do(ctx, req, instruments)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return instruments, resp, nil
 }
