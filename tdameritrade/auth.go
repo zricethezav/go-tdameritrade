68655d996ded95056c1b20db155c14f1777cb8f8
Jonathan Cooper <cooper@cooperthecoder.com>
2020-08-11 16:47:20 -0500

    Added auth code to abstract away TD Ameritrade's OAuth process while protecting against some OAuth attacks

--- tdameritrade/auth.go
+++ tdameritrade/auth.go
@@ -1,1 +1,118 @@
 package tdameritrade
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+
+	"golang.org/x/oauth2"
+)
+
+var (
+	// ErrNoCode is returned when the TD Ameritrade request is missing a code.
+	ErrNoCode = fmt.Errorf("missing code in request from TD Ameritrade")
+
+	// ErrNoState is returned when TD Ameritrade request is missing state, indicating a CSRF attempt.
+	ErrNoState = fmt.Errorf("missing state in request from TD Ameritrade")
+)
+
+// PersistentStore is meant to persist data from TD Ameritrade that is needed between requests.
+// Implementations must return the same value they set for a user in StoreState in GetState, or the login process will fail.
+// It is meant to allow credentials to be stored in cookies, JWTs and anything else you can think of.
+type PersistentStore interface {
+	StoreToken(token *oauth2.Token, w http.ResponseWriter, req *http.Request) error
+	GetToken(req *http.Request) (*oauth2.Token, error)
+	StoreState(state string, w http.ResponseWriter, req *http.Request) error
+	GetState(*http.Request) (string, error)
+}
+
+// Authenticator is a helper for TD Ameritrade's authentication.
+// It authenticates users and validates the state returned from TD Ameritrade to protect users from CSRF attacks.
+// It's recommended to use NewAuthenticator instead of creating this struct directly because TD Ameritrade requires Client IDs to be in the form clientid@AMER.OAUTHAP.
+// This is not immediately obvious from the documentation.
+// See https://developer.tdameritrade.com/content/authentication-faq
+type Authenticator struct {
+	Store  PersistentStore
+	OAuth2 oauth2.Config
+}
+
+// NewAuthenticator will automatically append @AMER.OAUTHAP to the client ID to save callers hours of frustration.
+func NewAuthenticator(store PersistentStore, oauth2 oauth2.Config) *Authenticator {
+	oauth2.ClientID = oauth2.ClientID + "@AMER.OAUTHAP"
+	return &Authenticator{
+		Store:  store,
+		OAuth2: oauth2,
+	}
+}
+
+// AuthenticatedClient tries to create an authenticated `Client` from a user's request
+func (a *Authenticator) AuthenticatedClient(ctx context.Context, req *http.Request) (*Client, error) {
+	token, err := a.Store.GetToken(req)
+	if err != nil {
+		return nil, err
+	}
+
+	authenticatedClient := a.OAuth2.Client(ctx, token)
+	return NewClient(authenticatedClient)
+}
+
+// StartOAuth2Flow returns TD Ameritrade's Auth URL and stores a random state value.
+// Redirect users to the returned URL to begin authentication.
+func (a *Authenticator) StartOAuth2Flow(w http.ResponseWriter, req *http.Request) (string, error) {
+	// Do not leave state generation up to callers.
+	// Experience has shown that people often do not know what OAuth2 state is and leave themselves vulnerable to CSRF attacks.
+	b := make([]byte, 32)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+
+	state := base64.RawURLEncoding.EncodeToString(b)
+	err := a.Store.StoreState(state, w, req)
+	if err != nil {
+		return "", err
+	}
+
+	return a.OAuth2.AuthCodeURL(state), nil
+}
+
+// FinishOAuth2Flow finishes authenticating a user returning from TD Ameritrade.
+// It verifies that TD Ameritrade has returned the expected state to prevent CSRF attacks and returns an authenticated `Client` on success.
+func (a *Authenticator) FinishOAuth2Flow(ctx context.Context, w http.ResponseWriter, req *http.Request) (*Client, error) {
+	code, ok := req.URL.Query()["code"]
+	if !ok || len(code) == 0 {
+		return nil, ErrNoCode
+	}
+	state, ok := req.URL.Query()["state"]
+	if !ok || len(state) == 0 {
+		return nil, ErrNoState
+	}
+
+	expectedState, err := a.Store.GetState(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Sanity check: this should never happen as long as users use StartOAuth2Flow.
+	// Prevent users from making themselves vulnerable to CSRF by forcing them to set state.
+	if len(expectedState) == 0 {
+		return nil, fmt.Errorf("state cannot be empty")
+	}
+
+	if state[0] != expectedState {
+		return nil, fmt.Errorf("invalid state. expected: '%v', got '%v'", expectedState, state[0])
+	}
+	token, err := a.OAuth2.Exchange(ctx, code[0])
+	if err != nil {
+		return nil, err
+	}
+
+	err = a.Store.StoreToken(token, w, req)
+	if err != nil {
+		return nil, err
+	}
+
+	authenticatedClient := a.OAuth2.Client(ctx, token)
+	return NewClient(authenticatedClient)
+}
---
+++ tdameritrade/auth_test.go
@@ -0,0 +1,148 @@
+package tdameritrade
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"time"
+
+	"golang.org/x/oauth2"
+)
+
+type testStore struct{}
+
+func (s *testStore) StoreToken(token *oauth2.Token, w http.ResponseWriter, req *http.Request) error {
+	return nil
+}
+
+func (s *testStore) GetToken(req *http.Request) (*oauth2.Token, error) {
+	return &oauth2.Token{
+		AccessToken:  "ACCESSTOKEN",
+		RefreshToken: "REFRESHTOKEN",
+		Expiry:       time.Now().AddDate(0, 0, 1),
+	}, nil
+}
+
+func (s *testStore) StoreState(state string, w http.ResponseWriter, req *http.Request) error {
+	return nil
+}
+
+func (s *testStore) GetState(req *http.Request) (string, error) {
+	return "state", nil
+}
+
+type testEmptyStore struct{}
+
+func (s *testEmptyStore) StoreToken(token *oauth2.Token, w http.ResponseWriter, req *http.Request) error {
+	return nil
+}
+
+func (s *testEmptyStore) GetToken(req *http.Request) (*oauth2.Token, error) {
+	return &oauth2.Token{
+		AccessToken:  "ACCESSTOKEN",
+		RefreshToken: "REFRESHTOKEN",
+		Expiry:       time.Now().AddDate(0, 0, 1),
+	}, nil
+}
+
+func (s *testEmptyStore) StoreState(state string, w http.ResponseWriter, req *http.Request) error {
+	return nil
+}
+
+func (s *testEmptyStore) GetState(req *http.Request) (string, error) {
+	return "", nil
+}
+
+func TestStartOAuth2ReturnsCorrectURL(t *testing.T) {
+	authenticator := &Authenticator{
+		Store: &testEmptyStore{},
+		OAuth2: oauth2.Config{
+			ClientID: "CLIENTID",
+			Endpoint: oauth2.Endpoint{
+				TokenURL: "https://api.tdameritrade.com/v1/oauth2/token",
+				AuthURL:  "https://auth.tdameritrade.com/auth",
+			},
+			RedirectURL: "https://localhost:8080/callback",
+		},
+	}
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/callback?code=code&state=state", nil)
+	u, err := authenticator.StartOAuth2Flow(w, req)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	expectedURL, _ := url.Parse("https://auth.tdameritrade.com/auth?client_id=CLIENTID&redirect_uri=https%3A%2F%2Flocalhost%3A8080%2Fcallback&response_type=code")
+	actualURL, _ := url.Parse(u)
+
+	// State query param is different each invocation.
+	// This cannot be changed by design.
+	modifiedQuery := actualURL.Query()
+	modifiedQuery.Del("state")
+	actualURL.RawQuery = modifiedQuery.Encode()
+	if actualURL.String() != expectedURL.String() {
+		t.Fatalf("invalid auth code URL. expected: '%v', got: '%v'", expectedURL, actualURL)
+	}
+}
+
+func TestFinishOAuth2RejectsEmptyState(t *testing.T) {
+	authenticator := &Authenticator{
+		Store: &testEmptyStore{},
+		OAuth2: oauth2.Config{
+			ClientID: "CLIENTID",
+			Endpoint: oauth2.Endpoint{
+				TokenURL: "https://api.tdameritrade.com/v1/oauth2/token",
+				AuthURL:  "https://auth.tdameritrade.com/auth",
+			},
+			RedirectURL: "https://localhost:8080/callback",
+		},
+	}
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/callback?code=code&state=state", nil)
+	ctx := context.Background()
+	client, err := authenticator.FinishOAuth2Flow(ctx, w, req)
+	if err == nil {
+		t.Fatalf("empty state not rejected.")
+	}
+
+	if err.Error() != "state cannot be empty" {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if client != nil {
+		t.Fatalf("client returned despite empty state.")
+	}
+}
+
+func TestFinishOAuth2RejectsInvalidState(t *testing.T) {
+	authenticator := &Authenticator{
+		Store: &testStore{},
+		OAuth2: oauth2.Config{
+			ClientID: "CLIENTID",
+			Endpoint: oauth2.Endpoint{
+				TokenURL: "https://api.tdameritrade.com/v1/oauth2/token",
+				AuthURL:  "https://auth.tdameritrade.com/auth",
+			},
+			RedirectURL: "https://localhost:8080/callback",
+		},
+	}
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/callback?code=code&state=invalid", nil)
+	ctx := context.Background()
+	client, err := authenticator.FinishOAuth2Flow(ctx, w, req)
+	if err == nil {
+		t.Fatalf("invalid state not rejected.")
+	}
+
+	if err.Error() != "invalid state. expected: 'state', got 'invalid'" {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if client != nil {
+		t.Fatalf("client returned despite invalid state.")
+	}
+}
